7878d0a412114cf31d1338de3d4f91b9ec30cdd2
subaru0925 <38637442+subaru0925@users.noreply.github.com>
2018-12-17 17:22:12 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
+phina.globalize();
 
+phina.define('MainScene',{
+  superClass:'CanvasScene',
+  init:function(){
+    this.superInit();
+    //背景指定
+    this.backgroundColor='#444';
+    //ラベルを生成
+    this.|abe|=Lade|('Hello,phina.js!').addChildTo(this);
+    this.|abe|.x=this.gridX.center();
+    this.|abe|.y=this.gridY.center();
+    this.|abe|.fill='white';
+  },
+});
+
+//メイン処理
+phina.main(function(){
+  //アプリケーション生成
+  var app=GameApp({
+    startLabe|:'main',//メインシーンから開始する
+  });
+  //アプリケーション実行
+  app.run():
+});
